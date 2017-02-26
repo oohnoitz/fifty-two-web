8ed21ed9e500841c16047ce839f8dd538bab12bb
Hung Tran <hung@0x0.ninja>
2017-02-26 04:05:27 -0600

    add webpack config and skeleton

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'babel-polyfill'
+
+ReactDOM.render(
+  <div/>
+, document.getElementById('main'))
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const MODULES = resolve(__dirname, 'node_modules')
+const APP_DIR = resolve(__dirname, 'app')
+const WEB_DIR = resolve(__dirname, 'public')
+const BUILD_DIR = resolve(__dirname, 'public', 'build')
+
+const isDebug = !process.argv.includes('--production')
+const enableHMR = !process.argv.includes('--no-hmr')
+
+const config = {
+  devtool: isDebug ? 'source-map' : false,
+
+  plugins: [
+
+  ],
+
+  resolve: {
+    extensions: ['.js', '.sass', '.scss'],
+    modules: ['node_modules'],
+  },
+
+  entry: [
+    `${APP_DIR}/main.js`,
+  ],
+
+  output: {
+    path: WEB_DIR,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [MODULES]
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [MODULES],
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+      {
+        test: /\.(svg|eot|ttf|woff|woff2)(\?[a-z0-9\=\.]+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+}
+
+if (isDebug && enableHMR) {
+  config.entry.unshift('webpack/hot/only-dev-server')
+  config.entry.unshift('webpack-dev-server/client?http://localhost:3000')
+  config.entry.unshift('react-hot-loader/patch')
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  config.plugins.push(new webpack.NamedModulesPlugin())
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin())
+
+  config.devServer = {
+    host: 'localhost',
+    port: 3000,
+    contentBase: WEB_DIR,
+    historyApiFallback: true,
+    hot: true,
+  }
+}
+
+module.exports = config
